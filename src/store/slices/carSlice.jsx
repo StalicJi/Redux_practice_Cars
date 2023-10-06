f7fffbf3abcb1dcae1cfb3e9f3e9f0e69dcfaadf
StalicJi <a7330831@gmail.com>
2023-10-06 22:54:15 +0800

    first commit

---
+++ src/store/slices/carSlice.jsx
@@ -0,0 +1,32 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const carsSlice = createSlice({
+    name: 'cars',
+    initialState: {
+        searchTrem : '',
+        data: []
+    },
+    reducers: {
+        changeSearchTerm(state, action) {
+            state.searchTrem = action.payload;
+        },
+        addCar(state, action) {
+            //提示：action.payload === { name:'abc', cost:140 } 
+            state.data.push({
+                name: action.payload.name,
+                cost: action.payload.cost,
+                id: nanoid(), //nanoid 是 Redux內建的隨機id
+            })
+        },
+        removeCar(state, action) {
+            //提示：action.payload === 符合 id 的 cars
+            const updated = state.data.filter((car) => {
+                return car.id !== action.payload
+            });
+            state.data = updated;
+        }
+    }
+});
+
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
+export const carsReducer = carsSlice.reducer;
