3b845c82ed392cb1ea5e2b667f202fe76c5892e7
StalicJi <a7330831@gmail.com>
2023-10-08 00:29:36 +0800

    20231008新增total cost

--- src/components/CarValue.jsx
+++ src/components/CarValue.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const CarValue = () => {
+
+  const totalCost = useSelector(({ cars : {data, searchTerm}}) => {
+    const filteredCars = data.filter((car) => {
+      return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    
+    return filteredCars.reduce((acc, car) => {
+      return acc + car.cost
+    }, 0)
+    
+  });
+
   return (
-    <div>
-      CarValue
+    <div className='car-value'>
+      Total Cost: ${totalCost}
     </div>
   )
 }
